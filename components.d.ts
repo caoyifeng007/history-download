de839d668308d07bb328bc33f7b01920ca8f88d0
ElvinsJ <caoyifeng007@163.com>
2022-02-08 15:16:08 +0800

    格式调整

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,7 @@ declare module 'vue' {
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDivider: typeof import('element-plus/es')['ElDivider']
     ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElRadio: typeof import('element-plus/es')['ElRadio']
@@ -24,4 +25,4 @@ declare module 'vue' {
   }
 }
 
-export { }
+export {}
