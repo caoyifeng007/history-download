97adca9a86b45e3f141260f85f8f26b4d2b0bc85
ElvinsJ <caoyifeng007@163.com>
2022-02-10 16:49:16 +0800

    添加vue模块声明和less库

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
   // 更多环境变量...
 }
+
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  const componentOptions: ComponentOptions
+  export default componentOptions
+}
