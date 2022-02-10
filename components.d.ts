a7d8a0213a8afe9fbaca7d9788b9f5d960eb07af
ElvinsJ <caoyifeng007@163.com>
2022-02-10 16:50:19 +0800

    添加element-plus icon库以及演示用法

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,8 @@ declare module 'vue' {
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDivider: typeof import('element-plus/es')['ElDivider']
     ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElInput: typeof import('element-plus/es')['ElInput']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
     ElRadio: typeof import('element-plus/es')['ElRadio']
@@ -24,4 +26,4 @@ declare module 'vue' {
   }
 }
 
-export {}
+export { }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from '@/router'
 
+import { Edit, Aim } from '@element-plus/icons-vue'
+
 const app = createApp(App)
+
+app.component('edit', Edit)
+app.component('aim', Aim)
 
 app.use(createPinia())
 app.use(router)
