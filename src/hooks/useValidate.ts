c8f0c6ba0241f6e23f9cac4abf95c10719d10cb1
ElvinsJ <caoyifeng007@163.com>
2022-03-24 17:10:47 +0800

    对yup校验错误时产生的error message进行localize

--- src/hooks/useValidate.ts
+++ src/hooks/useValidate.ts
@@ -1,42 +1,29 @@
-import { useForm, useField, configure } from 'vee-validate'
-import { localize } from '@vee-validate/i18n'
-import * as yup from 'yup'
+import { useForm, useField } from 'vee-validate'
+import { object, string, setLocale } from 'yup'
 
 export function useValidate() {
+  setLocale({
+    string: {
+      matches: '席位号不正确',
+    },
+
+    // use functions to generate an error object that includes the value from the schema
+    // number: {
+    //   min: ({ min }) => ({ key: 'field_too_short', values: { min } }),
+    //   max: ({ max }) => ({ key: 'field_too_big', values: { max } }),
+    // },
+  })
+
   // Define a validation schema
-  const schema = yup.object({
-    account: yup.string().required().min(10),
-    password: yup.string().required(),
+  const schema = object({
+    account: string().required().matches(/CS/).length(2),
+    // .matches(/CS\d{8}/),
+    password: string().required(),
   })
 
   // Create a form context with the validation schema
   useForm({
     validationSchema: schema,
-  })
-
-  configure({
-    generateMessage: localize({
-      en: {
-        names: {
-          account: '席位号',
-          password: '密码',
-        },
-      },
-    }),
-  })
-  // Or update multiple languages
-  localize({
-    en: {
-      fields: {
-        account: {
-          required: '席位号不能为空！',
-          min: '席位号不能为空！',
-        },
-        password: {
-          required: '密码不能为空！',
-        },
-      },
-    },
   })
 
   // No need to define rules for fields
