fbd35ca73fee17261cb1d708615d3ad436ecb7ee
ElvinsJ <caoyifeng007@163.com>
2022-05-03 01:04:27 +0800

    试验transition组件使用custom animation写法

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <transition
+  <!-- <transition
     enter-active-class="duration-500 ease-in-out"
     enter-from-class="translate-x-6 opacity-0"
     enter-to-class="translate-x-0 opacity-100"
@@ -8,8 +8,17 @@
     leave-to-class="translate-x-6 opacity-0"
   >
     <div v-show="t">111</div>
+  </transition> -->
+  <transition
+    type="animation"
+    enter-active-class="animate-wiggle"
+    leave-active-class="animate-wigglere"
+  >
+    <div v-show="t" class="inline-block">111</div>
   </transition>
+  <br />
   <button @click="t = !t">hit</button>
+  <!-- <div class="animate-wiggle">123</div> -->
 
   <el-config-provider :locale="zhCn">
     <div class="p-4 min-h-screen bg-gray-50">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,21 @@
 module.exports = {
   // purge: ['./index.html', './src/**/*.{js,ts,vue}'],
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  important: true,
+  // important: true,
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%': { transform: 'scale(0)' },
+          '50%': { transform: 'scale(1.5)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in',
+        wigglere: 'wiggle 1s ease-out reverse',
+      },
+    },
   },
   plugins: [],
 }
