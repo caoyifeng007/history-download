e0a412d223043df94da6e5b3fcdd26a31ee25e8a
ElvinsJ <caoyifeng007@163.com>
2022-05-31 16:46:28 +0800

    去除vite-plugin-html部分

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { createHtmlPlugin } from 'vite-plugin-html'
-
-// const title = import.meta.env.VITE_APP_TITLE
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,13 +14,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: '行情云历史分发',
-        },
-      },
-    }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
