5160aedd63e9bab80fe231592a8f56e914b7402b
ElvinsJ <caoyifeng007@163.com>
2022-06-13 16:27:47 +0800

    让tailwind导入位置靠后来覆盖EP的样式

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 
 import { store } from '@/stores'
 import { createHead } from '@vueuse/head'
-import '@/css/style.css'
 
 import App from '@/App.vue'
 import router from '@/router'
 
 import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
+// 导入位置靠后解决与EP样式冲突
+import '@/css/style.css'
 
 const app = createApp(App)
 const head = createHead()
